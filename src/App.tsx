f2c7c49e5f027dd1a562d2918a1b4bf9edfb086f
ifer-itcast <yangkangkang@itcast.cn>
2021-03-12 22:33:15 +0800

    加载媒体与字体

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './App.module.css';
+import logo from './assets/images/logo.svg'
 
 function App() {
-  return <div className={styles.red}>xxx</div>;
+  return <div className={styles.red}>
+    <img src={logo} width="200" alt=""/>
+    <h1>今天星期一，全校升国旗！</h1>
+  </div>;
 }
 
 export default App;
