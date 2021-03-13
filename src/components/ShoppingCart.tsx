1b10bc1575846a6e0f8c7fa4481906071c96f3ee
ifer-itcast <yangkangkang@itcast.cn>
2021-03-13 20:56:06 +0800

    React事件处理

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import styles from './ShoppingCart.module.css';
+import { FiShoppingCart } from 'react-icons/fi';
 
 interface Props {}
 
 interface State {
-  isOpen: boolean
+  isOpen: boolean;
 }
 
 export default class ShoppingCart extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
   }
+
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if ((e.target as HTMLElement).nodeName === 'SPAN') {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  };
+
   render() {
     return (
       <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={() => {
-          this.setState({
-            isOpen: !this.state.isOpen
-          });
-        }}>购物车</button>
-        <div className={styles.cartDropDown} style={{
-          display: this.state.isOpen ? 'block' : 'none'
-        }}>
+        <button className={styles.button} onClick={this.handleClick}>
+          <FiShoppingCart />
+          <span>购物车</span>
+        </button>
+        <div
+          className={styles.cartDropDown}
+          style={{
+            display: this.state.isOpen ? 'block' : 'none',
+          }}
+        >
           <ul>
-            <li></li>
+            <li />
           </ul>
         </div>
       </div>
-    )
+    );
   }
 }
