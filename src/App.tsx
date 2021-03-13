661d1f3654d9bf666e6bf7c8762b52760d044aef
ifer-itcast <yangkangkang@itcast.cn>
2021-03-13 22:32:36 +0800

    useEffect错误和loading处理

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,44 @@ interface State {
 
 const App: React.FC = props => {
   const [robotGallery, setRobotGallery] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
   useEffect(() => {
-    fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => setRobotGallery(data));
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setRobotGallery(data);
+      } catch (e) {
+        setError(e.message);
+      }
+
+      setLoading(false);
+    };
+    fetchData();
   }, []);
   return (
     <div className={styles.red}>
       <img src={logo} width="200" alt="" />
       <h1>今天星期一，全校升国旗！</h1>
       <ShoppingCart />
-      <ul style={{ textAlign: 'left', color: 'black', lineHeight: '30px' }}>
-        {robotGallery.map(r =>
-          <li key={r.id}>
-            name: {r.name}
-            email: {r.email}
-          </li>
-        )}
-      </ul>
+      {error &&
+        <div>
+          出错：{error}
+        </div>}
+      {!loading
+        ? <ul style={{ textAlign: 'left', color: 'black', lineHeight: '30px' }}>
+            {robotGallery.map(r =>
+              <li key={r.id}>
+                name: {r.name}
+                email: {r.email}
+              </li>
+            )}
+          </ul>
+        : <h2>loading...</h2>}
     </div>
   );
 };
