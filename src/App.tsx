2000052005ec606f6c54b27f039d63ae043621d1
ifer-itcast <yangkangkang@itcast.cn>
2021-03-13 21:41:26 +0800

    发送请求

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import styles from './App.module.css';
-import logo from './assets/images/logo.svg'
+import logo from './assets/images/logo.svg';
 import ShoppingCart from './components/ShoppingCart';
 
-function App() {
-  return <div className={styles.red}>
-    <img src={logo} width="200" alt=""/>
-    <h1>今天星期一，全校升国旗！</h1>
-    <ShoppingCart/>
-  </div>;
+interface Props {}
+interface State {
+  robotGallery: any[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+    };
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data =>
+      this.setState({
+        robotGallery: data,
+      })
+    );
+  }
+  render() {
+    return (
+      <div className={styles.red}>
+        <img src={logo} width="200" alt="" />
+        <h1>今天星期一，全校升国旗！</h1>
+        <ShoppingCart />
+        <ul style={{textAlign: 'left', color: 'black', lineHeight: '30px'}}>
+          {
+            this.state.robotGallery.map(r => <li key={r.id}>
+              name: {r.name}
+              email: {r.email}
+            </li>)
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
