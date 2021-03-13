6381daaab8a69bc6587893bada7c4d9fea713199
ifer-itcast <yangkangkang@itcast.cn>
2021-03-13 22:12:41 +0800

    useEffect异步获取数据

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import logo from './assets/images/logo.svg';
 import ShoppingCart from './components/ShoppingCart';
@@ -8,37 +8,26 @@ interface State {
   robotGallery: any[];
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-    };
-  }
-  componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data =>
-      this.setState({
-        robotGallery: data,
-      })
-    );
-  }
-  render() {
-    return (
-      <div className={styles.red}>
-        <img src={logo} width="200" alt="" />
-        <h1>今天星期一，全校升国旗！</h1>
-        <ShoppingCart />
-        <ul style={{textAlign: 'left', color: 'black', lineHeight: '30px'}}>
-          {
-            this.state.robotGallery.map(r => <li key={r.id}>
-              name: {r.name}
-              email: {r.email}
-            </li>)
-          }
-        </ul>
-      </div>
-    );
-  }
-}
+const App: React.FC = props => {
+  const [robotGallery, setRobotGallery] = useState<any>([]);
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => setRobotGallery(data));
+  }, []);
+  return (
+    <div className={styles.red}>
+      <img src={logo} width="200" alt="" />
+      <h1>今天星期一，全校升国旗！</h1>
+      <ShoppingCart />
+      <ul style={{ textAlign: 'left', color: 'black', lineHeight: '30px' }}>
+        {robotGallery.map(r =>
+          <li key={r.id}>
+            name: {r.name}
+            email: {r.email}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
